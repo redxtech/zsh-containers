dec2788d4f9ce779add1cc4e423baa39801ca74f
Gabe Dunn <gabe@sent.at>
2023-07-28 11:23:08 -0600

    fix: better command checking

--- containers.plugin.zsh
+++ containers.plugin.zsh
@@ -4,11 +4,11 @@
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 # if podman is installed, alias docker to podman, and load the podman wrapper function
-if ${+commands[podman]}; then
+if command -v podman >/dev/null; then
   alias docker="podman"
 
   # if podman-compose is available, load the wrapper function
-  if ${+commands[podman-compose]}; then
+  if command -v podman-compose >/dev/null; then
     fpath+=("${0:h}/functions")
     autoload -Uz podman
   fi
